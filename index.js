5985bd019b2a28aea4d12ca43ee59d9ef4957696
MayteeAkk <mayteeoakk@gmail.com>
2022-04-30 01:46:45 -0400

    Another attempt

--- index.js
+++ index.js
@@ -1,1 +1,157 @@
 // Place your server entry point code here
+const express = require('express')
+const app = express()
+const db = require("./src/services/database.js")
+const morgan = require('morgan')
+const fs = require ('fs')
+
+// Serve static HTML files
+app.use(express.static('./public'));
+
+// Make Express use its own built-in body parser to handle JSON
+app.use(express.json());
+
+const args = require('minimist')(process.argv.slice(2));
+const port = args.port || process.env.PORT || 5555
+
+// Store help text 
+const help = (`
+server.js [options]
+
+--port	Set the port number for the server to listen on. Must be an integer
+            between 1 and 65535.
+
+--debug	If set to true, creates endlpoints /app/log/access/ which returns
+            a JSON access log from the database and /app/error which throws 
+            an error with the message "Error test successful." Defaults to 
+            false.
+
+--log		If set to false, no log files are written. Defaults to true.
+            Logs are always written to database.
+
+--help	Return this message and exit.
+`)
+// If --help or -h, echo help text to STDOUT and exit
+if (args.help || args.h) {
+    console.log(help)
+    process.exit(0)
+}
+
+// Start an app server
+const server = app.listen(port, () => {
+    console.log('App listening on port %PORT%'.replace('%PORT%', port))
+});
+
+app.use( (req, res, next) => {
+    // Your middleware goes here.
+    let logdata = {
+        remoteaddr: req.ip,
+        remoteuser: req.user,
+        time: Date.now(),
+        method: req.method,
+        url: req.url,
+        protocol: req.protocol,
+        httpversion: req.httpVersion,
+        status: res.statusCode,
+        referer: req.headers['referer'],
+        useragent: req.headers['user-agent']
+    }
+    const stmt = db.prepare('INSERT INTO accesslog (remoteaddr, remoteuser, time, method, url, protocol, httpversion, status, referer, useragent) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)') 
+    stmt.run(logdata.remoteaddr, logdata.remoteuser, logdata.time, logdata.method, logdata.url, logdata.protocol, logdata.httpversion, logdata.status, logdata.referer, logdata.useragent) 
+    next();
+})
+
+// Debug endpoints
+if (args.debug) {
+    app.get('/app/log/access', (req, res) => {
+        const stmt = db.prepare('SELECT * from accesslog').all()
+        res.status(200).json(stmt)
+    });
+
+    app.get('/app/error', (req, res) => {
+        throw new Error('Error test successful.')
+    });
+}
+
+// If log == True
+if (args.log) {
+    const writestream = fs.createWriteStream('./access.log', { flags: 'a'})
+    app.use(morgan('combined'), {stream: writestream})
+}
+
+app.get('/app', (req, res) => {
+    // Respond with status 200
+    res.statusCode = 200;
+    // Respond with status message "OK"
+    res.statusMessage = 'OK';
+    res.writeHead(res.statusCode, { 'Content-Type' : 'text/plain' });
+    res.status(res.statusCode).end(res.statusCode + ' ' + res.statusMessage)
+})
+
+app.get('/app/flip', (req, res) => {
+    var flip = coinFlip()
+    res.type('json')
+    res.status(200).json({"flip": flip})
+})
+
+app.post('/app/flip/coins/', (req, res, next) => {
+    const flips = coinFlips(req.body.number)
+    const count = countFlips(flips)
+    res.status(200).json({"raw":flips,"summary":count})
+})
+
+app.post('/app/flip/call/', (req, res, next) => {
+    const game = flipACoin(req.body.guess)
+    res.status(200).json(game)
+})
+
+// Default response for any other request
+app.use(function(req, res) {
+    res.type('text/plain')
+    res.status(404).send('404 NOT FOUND')
+})
+
+// Coin Flip Functions
+function coinFlip() {
+    if (Math.random() < 0.5) {
+        return 'heads';
+    }
+        return 'tails';
+}
+
+function coinFlips(flips) {
+    let flipSet = new Array(flips);
+    let x = 0;
+    while (x < flips) {
+        flipSet[x] = coinFlip();
+        x = x + 1;
+    }
+    return flipSet;
+}
+
+function countFlips(array) {
+    let headCount = 0;
+    let tailsCount = 0;
+    for (let x = 0; x < array.length; x++) {
+        if (array[x] == 'heads') {
+            headCount++;
+        }
+        else {tailsCount++;}
+    }
+    if (headCount == 0) {return {tails: tailsCount}}
+    if (tailsCount == 0) {return {heads: headCount}}
+    else {
+        return {heads: headCount, tails: tailsCount};
+    }
+    
+}
+
+function flipACoin(call) {
+    const flipResult = coinFlip();
+    if (flipResult == call) {
+        return {call: call, flip: flipResult, result: 'win'};
+    }
+    else {
+        return {call: call, flip: flipResult, result: 'lose'}
+    }
+}
--- public/main.js
+++ public/main.js
@@ -1,78 +1,92 @@
-// Focus div based on nav button click
-function addNavButtonListener(button, focusID, func){
-    button.addEventListener("click", function(){
-        let focus = document.getElementById(focusID);
-        console.log(focus);
-        let active = document.getElementsByClassName("active");
-        func(focus);
+// // Focus div based on nav button click
+// function addNavButtonListener(button, focusID, func){
+//     button.addEventListener("click", function(){
+//         let focus = document.getElementById(focusID);
+//         console.log(focus);
+//         let active = document.getElementsByClassName("active");
+//         func(focus);
 
-        document.getElementById("home").className = "hidden";
-        activateFocusDiv(active, focus);
-    });
-}
+//         document.getElementById("home").className = "hidden";
+//         activateFocusDiv(active, focus);
+//     });
+// }
 
-function activeFocusDiv(divs, newFocus){
-    for(let i = 0; i < divs.length; i++){
-        divs[i].className = "hidden";
-    }
-    newFocus.className = newFocus.id == "home" ? "" : "active";
-    newFocus.className = "active";
-}
+// function activeFocusDiv(divs, newFocus){
+//     for(let i = 0; i < divs.length; i++){
+//         divs[i].className = "hidden";
+//     }
+//     newFocus.className = newFocus.id == "home" ? "" : "active";
+//     newFocus.className = "active";
+// }
 
-async function getData(url = "", data = {}) {
-    let response = await fetch(url, {
-        method: "GET",
-        headers:{
-            "Content-Type": "application/json"
-        },
-        redirect: "follow",
-        referrerPolicy: "no-referrer"
-    });
+// async function getData(url = "", data = {}) {
+//     let response = await fetch(url, {
+//         method: "GET",
+//         headers:{
+//             "Content-Type": "application/json"
+//         },
+//         redirect: "follow",
+//         referrerPolicy: "no-referrer"
+//     });
 
-    return response.json();
-}
+//     return response.json();
+// }
 
-async function postData(url = "", data = {}) {
-    let response = await fetch(url, {
-        method: "POST",
-        headers:{
-            "Content-Type": "application/json"
-        },
-        redirect: "follow",
-        referrerPolicy: "no-referrer",
-        body: JSON.stringify(data)
-    });
+// async function postData(url = "", data = {}) {
+//     let response = await fetch(url, {
+//         method: "POST",
+//         headers:{
+//             "Content-Type": "application/json"
+//         },
+//         redirect: "follow",
+//         referrerPolicy: "no-referrer",
+//         body: JSON.stringify(data)
+//     });
 
-    return response.json();
-}
+//     return response.json();
+// }
 
-function createImageElement(filename, className){
-    return `<img src="${filename}" class="${className || "smallcoin"}">`
-}
+// function createImageElement(filename, className){
+//     return `<img src="${filename}" class="${className || "smallcoin"}">`
+// }
 
-function addCoin(result){
-    if (result === "heads"){
-        return createImageElement("assets/img/heads.png")
-    }
+// function addCoin(result){
+//     if (result === "heads"){
+//         return createImageElement("assets/img/heads.png")
+//     }
     
-    if (result === "tails"){
-        return createImageElement("assets/img/tails.png")
-    }
-    return focus.innerHTML = createImageElement("assets/img/coin.png")
-}
+//     if (result === "tails"){
+//         return createImageElement("assets/img/tails.png")
+//     }
+//     return focus.innerHTML = createImageElement("assets/img/coin.png")
+// }
 
-let home = document.getElementById("homeNav");
-let single = document.getElementById("singleNav");
-let multi = document.getElementById("multiNav");
-let guess = document.getElementById("guessNav");
+// let home = document.getElementById("homeNav");
+// let single = document.getElementById("singleNav");
+// let multi = document.getElementById("multiNav");
+// let guess = document.getElementById("guessNav");
 
-// Flip one coin and show coin image to match result when button clicked
-function addCoinButtonListener(button, coinID, func){
+// // Flip one coin and show coin image to match result when button clicked
+// function addCoinButtonListener(button, coinID, func){
 
-}
+// }
 
-// Flip multiple coins and show coin images in table as well as summary results
-// Enter number and press button to activate coin flip series
+// // Flip multiple coins and show coin images in table as well as summary results
+// // Enter number and press button to activate coin flip series
 
-// Guess a flip by clicking either heads or tails button
+// // Guess a flip by clicking either heads or tails button
+
+const coin = document.getElementById("flipOne")
+coin.addEventListener("click", flipCoin)
+async function flipCoin() {
+    const endpoint = "app/flip/"
+    const url = document.baseURI+endpoint
+    await fetch(url).then(function(response) {
+    		    return response.json();
+  		      }).then(function(result) {
+				    console.log(result);
+				    document.getElementById("result").innerHTML = result.flip;
+				    document.getElementById("quarter").setAttribute("src", "assets/img/"+result.flip+".png");
+				  });
+};
 
